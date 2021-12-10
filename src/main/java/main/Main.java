ac4b66284190fc35205e7f49be8d5eab6871ba69
Said Choquehuanca <39719094+SaidChoquehuanca@users.noreply.github.com>
2021-12-09 22:26:42 -0500

    final2.0

--- src/main/java/main/Main.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-import java.util.List;
-
-public class Main {
-}
--- src/test/java/main/MainTest.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import main.Main.*;
-
-import java.util.logging.Logger;
-import java.io.IOException;
-
-public class MainTest {
-}
